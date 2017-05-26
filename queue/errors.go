64419e5284176c8d24276ae4ded04ad60eba1864
zliu <zhanliangliu@qq.com>
2017-05-26 11:45:46 +0800

    add: persistent message queue on top of leveldb

---
+++ queue/errors.go
@@ -0,0 +1,9 @@
+package queue
+
+import "errors"
+
+var (
+	ErrDBClosed    = errors.New("queue: database is closed")
+	ErrEmpty       = errors.New("queue: queue is empty")
+	ErrOutOfBounds = errors.New("queue: ID is outside range of queue")
+)
