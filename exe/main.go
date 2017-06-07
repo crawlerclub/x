e01b143fbd86e3a23fed9d75f7661b8055ad353a
zliu <zhanliangliu@qq.com>
2017-06-07 16:16:17 +0800

    newsmth.net crawler

---
+++ exe/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"github.com/crawlerclub/x/controller"
+	"github.com/crawlerclub/x/crawler"
+	"github.com/golang/glog"
+)
+
+func main() {
+	defer glog.Flush()
+	defer glog.Info("CRAWLER exit")
+
+	workerCnt := flag.Int("wc", 1000, "worker count")
+	confFile := flag.String("conf", "./www.newsmth.net.json", "crawler conf file")
+	runDir := flag.String("run", "./run", "working dir")
+	flag.Parse()
+
+	var controller controller.Controller
+	controller.Init(*runDir, *workerCnt)
+
+	var crawler crawler.Crawler
+	err := crawler.LoadConfFromFile(*confFile)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	controller.Crawlers[crawler.Conf.CrawlerName] = crawler
+	tasks, err := crawler.GetStartTasks()
+	if err != nil {
+		glog.Fatal(err)
+	}
+	for _, task := range tasks {
+		if _, err := controller.TaskQueue.EnqueueObject(task); err != nil {
+			glog.Fatal(err)
+		}
+	}
+	glog.Info("run!")
+	controller.Run()
+}
